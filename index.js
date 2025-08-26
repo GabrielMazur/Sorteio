6d8b776695e2084fac94eb76b18dbcd2a0b8303d
GabrielMazur <162588822+GabrielMazur@users.noreply.github.com>
2025-08-26 09:18:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let amigos =[];
-document.getElementById('Adicionar').onclick = function(){
-    let amigo = document.getElementById('nome').value.trim();
-    if(amigo){
-        amigos.push(amigo)
-        for(let i = 0; i < amigos.length; i++){
-            document.getElementById('lista').innerHTML = `${amigos[i]}`
+let amigos = [];
+
+        // Ao clicar em "Adicionar"
+        document.getElementById("btnAdicionar").onclick = function() {
+            let nome = document.getElementById("amigo").value.trim();
+            if (nome) {
+                amigos.push(nome);
+                document.getElementById("amigo").value = "";
+
+                document.getElementById("listaAmigos").innerHTML = "";
+                for (let i = 0; i < amigos.length; i++) {
+                    document.getElementById("listaAmigos").innerHTML += `<li>${amigos[i]}</li>`;
+                }
+            }
         }
-    }
-}
+
+        document.getElementById('sortear').onclick = function(){
+            let sorteado = compras[Math.floor(Math.random() * compras.length)];
+            document.getElementById('lista2').innerHTML = `<li>O sorteado é : ${sorteado}</li>`
+        }
